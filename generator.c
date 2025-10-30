88ddbab05d6493663c34bb18fe69de7bf85783aa
S-Grow <spencer.grow@maine.edu>
2025-10-29 22:09:59 -0400

    Voltage Monitoring Initial Setup
    Added VoltageMonitoring.c, generartor.h, and generator.c, for VoltageMonitoring.c included basic overview and layout for voltage monitoring on inputs.

---
+++ generator.c
@@ -0,0 +1,5 @@
+#include "generator.h"
+#include <stdio.h>
+#include "stm32c0xx_hal.h" // HAL functions
+
+//gen startup sim
---
+++ generator.h
@@ -0,0 +1,10 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "main.h"
+
+// Functions to control generator
+void generator_startup_sequence(void); // Full startup sequence
+void generator_stop(void);             // Stop generator immediately
+
+#endif
